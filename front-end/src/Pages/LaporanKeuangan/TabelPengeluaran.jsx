a4e95a6ad65cb474aa276e0e5a783998e41b834e
kikanindya <kikanbae03@gmail.com>
2025-12-11 14:15:57 +0700

    Add files via upload

--- front-end/src/Pages/LaporanKeuangan/TabelPengeluaran.jsx
+++ front-end/src/Pages/LaporanKeuangan/TabelPengeluaran.jsx
@@ -8,32 +8,34 @@ const TabelPengeluaran = () => {
   ];
 
   return (
-    <div className="bg-white p-6 rounded-lg shadow-md mb-6 relative">
-      <div className="bg-white border border-pink-300 mb-8 p-4 rounded-lg shadow-md shadow-pink-200/50">
-        <input type="date" defaultValue="" className="rounded px-2 py-1 w-full" />
+    <div className="w-full h-full">
+      <div className="bg-white p-6 rounded-lg shadow-md mb-6 relative min-h-[400px]">
+        <div className="bg-white border border-pink-300 mb-8 p-4 rounded-lg shadow-md shadow-pink-200/50">
+          <input type="date" defaultValue="" className="rounded px-2 py-1 w-full" />
+        </div>
+
+        <table className="w-full table-auto text-xs border-collapse">
+          <thead className="sticky top-0 bg-pink-100">
+            <tr>
+              <th className="border px-2 py-1">Deskripsi</th>
+              <th className="border px-2 py-1">Tanggal</th>
+              <th className="border px-2 py-1">Jumlah Pengeluaran</th>
+            </tr>
+          </thead>
+          <tbody>
+            {menuItems.map((item, index) => (
+              <tr key={index} className="even:bg-gray-50">
+                <td className="border px-2 py-1">{item.desc}</td>
+                <td className="border px-2 py-1">{item.tanggal}</td>
+                <td className="border px-2 py-1">{item.qty}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
 
-      <table className="w-full table-auto text-xs border-collapse">
-        <thead className="sticky top-0 bg-pink-100">
-          <tr>
-            <th className="border px-2 py-1">Deskripsi</th>
-            <th className="border px-2 py-1">Tanggal</th>
-            <th className="border px-2 py-1">Jumlah Pengeluaran</th>
-          </tr>
-        </thead>
-        <tbody>
-          {menuItems.map((item, index) => (
-            <tr key={index} className="even:bg-gray-50">
-              <td className="border px-2 py-1">{item.desc}</td>
-              <td className="border px-2 py-1">{item.tanggal}</td>
-              <td className="border px-2 py-1">{item.qty}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-
       <button onClick={() => window.history.back()}
-        className="mt-4 px-4 py-2 bg-pink-500 text-white rounded hover:bg-pink-600">
+        className="px-4 py-2 bg-pink-500 text-white rounded hover:bg-pink-600">
         Kembali
       </button>
     </div>
